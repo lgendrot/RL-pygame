6ddf601013451caa84ed0efa34bbd4caf6129e52
Luc Gendrot <lgendrot@gmail.com>
2020-03-29 20:23:35 -0500

    Add walls generated with text file and: + clean up junk files

--- gridland/main.py
+++ gridland/main.py
@@ -5,6 +5,8 @@
 
 import pygame as pg
 import sys
+
+from os import path
 from settings import *
 from sprites import *
 
@@ -14,20 +16,25 @@ class Game:
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100) # Allows us to hold down keys to move, etc.
+        pg.key.set_repeat(250, 50) # Allows us to hold dowW keys to move, etc.
         self.load_data() # For saving games later
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, "map.txt")) as f:
+            for line in f:
+                self.map_data.append(line)
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         self.player = Player(self, 10, 10)
-        for x in range(10, 20): # Gets taken out later
-            Wall(self, x, 5)
-
+        for row, tiles in enumerate(self.map_data):
+            for column, tile in enumerate(tiles):
+                if tile == "1":
+                    Wall(self, column, row) 
     def run(self):
         # game loop - set self.playing = False to end the game
         # useful for ending a training session
--- gridland/sprites.py
+++ gridland/sprites.py
@@ -6,15 +6,22 @@ class Player(pg.sprite.Sprite):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image = pg.Surface((TILESIZE, TILESIZE)) # Sprite class expects self.image
         self.image.fill(YELLOW)
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() # Sprite class expects self.rect
         self.x = x
         self.y = y
 
     def move(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
+        if not self.collide_with_walls(dx, dy):
+            self.x += dx
+            self.y += dy
+
+    def collide_with_walls(self, dx, dy):
+        for wall in self.game.walls:
+            if self.x + dx == wall.x and self.y + dy == wall.y:
+                return True
+        return False
 
     def update(self):
         self.rect.x = self.x * TILESIZE
